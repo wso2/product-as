609a7a298cfa61d48da0c8a40d78709e4c3637fe
Kasun Gajasinghe <kasung@wso2.com>
2015-04-24 17:09:35 +0530

    fixing build failure

--- modules/samples/product/FlickrClient/src/org/wso2/appserver/sample/flickr/client/Client.java
+++ modules/samples/product/FlickrClient/src/org/wso2/appserver/sample/flickr/client/Client.java
@@ -29,7 +29,7 @@ public class Client {
         frame.setEnabled(false);
         frame.show();
         frame.getAPIKEY();
-        frame.getSharedSecret()
+        frame.getSharedSecret();
         frame.setEnabled(true);
     }
 }
