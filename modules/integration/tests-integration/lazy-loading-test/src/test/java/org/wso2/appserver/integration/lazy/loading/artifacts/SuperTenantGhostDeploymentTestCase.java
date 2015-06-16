89483c07ba4ae222b5208cff65d0b8e73cc7556e
sanethd <sanethd@wso2.com>
2015-06-16 13:32:39 +0530

    enable the SuperTenantGhostDeploymentTestCase.java test case

--- modules/integration/tests-integration/lazy-loading-test/src/test/java/org/wso2/appserver/integration/lazy/loading/artifacts/SuperTenantGhostDeploymentTestCase.java
+++ modules/integration/tests-integration/lazy-loading-test/src/test/java/org/wso2/appserver/integration/lazy/loading/artifacts/SuperTenantGhostDeploymentTestCase.java
@@ -135,8 +135,8 @@ public class SuperTenantGhostDeploymentTestCase extends LazyLoadingBaseTest {
 
     @Test(groups = "wso2.as.lazy.loading", description = "Send a Get request after a web app is auto unload  and reload" +
             " in to Ghost form. After access web app, it should be in fully load form  the Ghost form",
-            dependsOnMethods = "testInvokeWebAppInGhostDeploymentOnSuperTenant", enabled = false)
-    public void testWebAppAutoUnLoadAndInvokeInGhostDeploymentOnSupperTenant() throws LazyLoadingTestException {
+            dependsOnMethods = "testInvokeWebAppInGhostDeploymentOnSuperTenant")
+    public void testWebAppAutoUnLoadAndInvokeInGhostDeploymentOnSuperTenant() throws LazyLoadingTestException {
         assertTrue(checkWebAppAutoUnloadingToGhostState(superTenantDomain, WEB_APP_FILE_NAME1),
                 "Web-app is not un-loaded ane re-deployed in Ghost form after idle time pass in super tenant " +
                         "Web_app Name: " + WEB_APP_FILE_NAME1);
@@ -162,7 +162,7 @@ public class SuperTenantGhostDeploymentTestCase extends LazyLoadingBaseTest {
     @Test(groups = "wso2.as.lazy.loading", description = "Test web application auto unload  and reload in Ghost" +
             " format. After access web app, it should be in fully load form  but after configured web app idle time pass" +
             " it should get auto unload ne reload in Ghost form.",
-            dependsOnMethods = "testInvokeWebAppInGhostDeploymentOnSuperTenant")
+            dependsOnMethods = "testWebAppAutoUnLoadAndInvokeInGhostDeploymentOnSuperTenant")
     public void testWebAppAutoUnLoadAndReloadInGhostFormInGhostDeploymentOnSuperTenant() throws Exception {
         serverManager.restartGracefully();
         HttpResponse httpResponse = HttpURLConnectionClient.sendGetRequest(webApp1URL, null);
