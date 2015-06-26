54caf199385feabaf4ae9be9b0d22e5f2cbb2a36
supunmalinga <awsmalinga@gmail.com>
2015-06-26 12:45:36 +0530

    enabling tenant test for CDI alternatives test

--- modules/integration/tests-integration/tests/src/test/java/org/wso2/appserver/integration/tests/javaee/cdi/CdiAlternativesTestCase.java
+++ modules/integration/tests-integration/tests/src/test/java/org/wso2/appserver/integration/tests/javaee/cdi/CdiAlternativesTestCase.java
@@ -17,42 +17,78 @@ package org.wso2.appserver.integration.tests.javaee.cdi;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
-import org.wso2.appserver.integration.tests.javaee.WebappDeploymentTestCase;
+import org.wso2.appserver.integration.common.utils.ASIntegrationTest;
+import org.wso2.appserver.integration.common.utils.WebAppDeploymentUtil;
+import org.wso2.appserver.integration.common.utils.WebAppTypes;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
 import org.wso2.carbon.automation.test.utils.http.client.HttpRequestUtil;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 
+import java.io.File;
+
 import static org.testng.Assert.assertTrue;
 
-public class CdiAlternativesTestCase extends WebappDeploymentTestCase {
+public class CdiAlternativesTestCase extends ASIntegrationTest {
 
     private static final Log log = LogFactory.getLog(CdiAlternativesTestCase.class);
 
     private static final String webAppFileName = "cdi-alternatives.war";
-    private static final String webAppFilePath = "cdi";
     private static final String webAppName = "cdi-alternatives";
     private static final String webAppLocalURL = "/cdi-alternatives";
+    String hostname;
+    private TestUserMode userMode;
+
+    @Factory(dataProvider = "userModeProvider")
+    public CdiAlternativesTestCase(TestUserMode userMode) {
+        this.userMode = userMode;
+    }
+
+    @DataProvider
+    private static TestUserMode[][] userModeProvider() {
+        return new TestUserMode[][]{
+                new TestUserMode[]{TestUserMode.SUPER_TENANT_ADMIN},
+                new TestUserMode[]{TestUserMode.TENANT_USER},
+        };
+    }
 
     @BeforeClass(alwaysRun = true)
     public void init() throws Exception {
-        super.init();
-        setWebAppFileName(webAppFileName);
-        setWebAppFilePath(webAppFilePath);
-        setWebAppName(webAppName);
-        setWebAppURL(getWebAppURL() + webAppLocalURL);
+        super.init(userMode);
+
+        hostname = asServer.getInstance().getHosts().get("default");
+        webAppURL = getWebAppURL(WebAppTypes.WEBAPPS) + webAppLocalURL;
+
+        String webAppFilePath = FrameworkPathUtil.getSystemResourceLocation() + "artifacts" + File.separator +
+                "AS" + File.separator + "javaee" + File.separator + "cdi" + File.separator + webAppFileName;
+        WebAppDeploymentUtil.deployWebApplication(backendURL, sessionCookie, webAppFilePath);
+
+        assertTrue(WebAppDeploymentUtil.isWebApplicationDeployed(backendURL, sessionCookie, webAppName),
+                "Web Application Deployment failed");
     }
 
-    @Test(groups = "wso2.as", description = "test cdi alternatives with servlet", dependsOnMethods = "webApplicationDeploymentTest")
+    @Test(groups = "wso2.as", description = "test cdi alternatives with servlet")
     public void testCdiServlet() throws Exception {
 
-        String servletUrl = getWebAppURL();
-        HttpResponse response = HttpRequestUtil.sendGetRequest(servletUrl, null);
+        String servletUrl = webAppURL;
+        HttpResponse response = HttpRequestUtil.sendGetRequest(webAppURL, null);
         String result = response.getData();
 
         log.info("Response - " + result);
 
         assertTrue(result.startsWith("Have a good day, Bye!"),
                 "Response doesn't contain the alternative's greeting " + servletUrl);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void cleanupWebApps() throws Exception {
+        WebAppDeploymentUtil.unDeployWebApplication(backendURL, hostname, sessionCookie, webAppFileName);
+        assertTrue(WebAppDeploymentUtil.isWebApplicationUnDeployed(backendURL, sessionCookie, webAppName),
+                "Web Application unDeployment failed");
     }
 }
