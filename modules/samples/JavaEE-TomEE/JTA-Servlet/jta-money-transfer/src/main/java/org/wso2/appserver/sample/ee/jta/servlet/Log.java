dce33a00557736eee5deed43137a95fb276b2ab0
supunmalinga <awsmalinga@gmail.com>
2015-06-30 10:14:15 +0530

    Adding log support for jra servlet sample

--- modules/samples/JavaEE-TomEE/JTA-Servlet/jta-money-transfer/src/main/java/org/wso2/appserver/sample/ee/jta/servlet/Log.java
+++ modules/samples/JavaEE-TomEE/JTA-Servlet/jta-money-transfer/src/main/java/org/wso2/appserver/sample/ee/jta/servlet/Log.java
@@ -1,7 +1,11 @@
 package org.wso2.appserver.sample.ee.jta.servlet;
 
-import javax.persistence.*;
 import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Version;
 
 @Entity
 public class Log {
--- modules/samples/JavaEE-TomEE/JTA-Servlet/jta-money-transfer/src/main/java/org/wso2/appserver/sample/ee/jta/servlet/Servlet.java
+++ modules/samples/JavaEE-TomEE/JTA-Servlet/jta-money-transfer/src/main/java/org/wso2/appserver/sample/ee/jta/servlet/Servlet.java
@@ -1,12 +1,12 @@
 package org.wso2.appserver.sample.ee.jta.servlet;
 
+import java.io.IOException;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(name = "MoneyTransferServlet", urlPatterns = "/")
 public class Servlet extends HttpServlet {
