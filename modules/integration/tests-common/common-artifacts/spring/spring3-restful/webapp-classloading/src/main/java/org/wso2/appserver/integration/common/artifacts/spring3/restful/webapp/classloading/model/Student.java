7bb16ed159d723cf3605d03b2810e9f0666d6123
Niranjan-K <niranjan.karu@gmail.com>
2015-07-01 08:21:25 +0530

    Rename Spring 3 package name

---
+++ modules/integration/tests-common/common-artifacts/spring/spring3-restful/webapp-classloading/src/main/java/org/wso2/appserver/integration/common/artifacts/spring3/restful/webapp/classloading/model/Student.java
@@ -0,0 +1,59 @@
+/*
+ *   Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ */
+
+package org.wso2.appserver.integration.common.artifacts.spring3.restful.webapp.classloading.model;
+
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
