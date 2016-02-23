a53c425f8ac352dd52443a70b7c3dc9d1f2468d7
Miraj Abeysekara <miraj@wso2.com>
2016-02-23 11:22:08 +0530

    defined the version numbers as properties in the pom's

--- samples/hello-world/src/main/java/org/wso2/appserver/sample/HelloWorldServlet.java
+++ samples/hello-world/src/main/java/org/wso2/appserver/sample/HelloWorldServlet.java
@@ -18,13 +18,16 @@
 
 package org.wso2.appserver.sample;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
+/**
+ *  Sample servlet containing a hello world as response.
+ */
 public class HelloWorldServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
