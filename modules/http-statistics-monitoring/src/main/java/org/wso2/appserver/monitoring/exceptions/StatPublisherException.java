0d7505abeec4ebf8aeee69dd325295bca825cb18
chirangaalwis <chirangaalwis@gmail.com>
2016-03-29 16:40:06 +0530

    Fixed comments in stat-publisher custom exception class.

--- modules/http-statistics-monitoring/src/main/java/org/wso2/appserver/monitoring/exceptions/StatPublisherException.java
+++ modules/http-statistics-monitoring/src/main/java/org/wso2/appserver/monitoring/exceptions/StatPublisherException.java
@@ -15,16 +15,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.wso2.appserver.monitoring.exceptions;
 
 /**
- * General exception class for the Statistics Publisher module
+ * General exception class for the Statistics Publisher module.
+ *
+ * @since 6.0.0
  */
 public class StatPublisherException extends Exception {
 
     /**
-     * Exception to be thrown when an error occurs in the statistics publisher module
+     * Exception to be thrown when an error occurs in the statistics publisher module.
      *
      * @param message the detail message
      * @param cause   the cause of exception
