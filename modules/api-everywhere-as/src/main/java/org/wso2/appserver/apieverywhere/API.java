10432f46d78ac020f1933f6462d3fd80f6499f93
senthalan <senthalank@gmail.com>
2016-09-01 11:57:06 +0530

    scanning annotations from class files and creating API

---
+++ modules/api-everywhere-as/src/main/java/org/wso2/appserver/apieverywhere/API.java
@@ -0,0 +1,54 @@
+package org.wso2.appserver.apieverywhere;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ */
+public class API {
+    private Consumes consume;
+    private Produces produce;
+    private Annotation type;
+    private String baseUrl;
+    private Path path;
+
+
+    public API(String baseUrl, Method me) {
+        if (me.getAnnotation(GET.class) != null) {
+            type = me.getAnnotation(GET.class);
+        }
+        if (me.getAnnotation(POST.class) != null) {
+            type = me.getAnnotation(POST.class);
+        }
+        if (me.getAnnotation(PUT.class) != null) {
+            type = me.getAnnotation(PUT.class);
+        }
+        if (me.getAnnotation(DELETE.class) != null) {
+            type = me.getAnnotation(DELETE.class);
+        }
+
+        consume = me.getDeclaredAnnotation(Consumes.class);
+        produce = me.getDeclaredAnnotation(Produces.class);
+
+
+        path = me.getAnnotation(Path.class);
+        this.baseUrl = baseUrl + path.value();
+    }
+
+
+    public String toString() {
+        return ("API:"
+                + "\n baseUrl- " + baseUrl
+                + "\n type- " + type
+                + "\n produces- " + produce
+                + "\n consumes- " + consume);
+    }
+}
--- modules/api-everywhere-as/src/main/java/org/wso2/appserver/apieverywhere/WebAppDeploymentListener.java
+++ modules/api-everywhere-as/src/main/java/org/wso2/appserver/apieverywhere/WebAppDeploymentListener.java
@@ -1,28 +1,58 @@
 package org.wso2.appserver.apieverywhere;
 
+
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
+import org.reflections.Reflections;
+import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeAnnotationsScanner;
 
+import java.lang.reflect.Method;
+import java.util.Set;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
+import javax.ws.rs.Path;
+
 
 
 /**
  *
  */
-@WebListener
 public class WebAppDeploymentListener implements ServletContextListener {
     private static final Log log = LogFactory.getLog(WebAppDeploymentListener.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         //APIs of web application have to be extracted
-        log.info("A web application is deployed");
         ServletContext servletContext = servletContextEvent.getServletContext();
-        ClassLoader classLoader = servletContext.getClassLoader();
-        log.info("class loading :::" + classLoader.toString());
+        StringBuffer baseUrl = new StringBuffer(servletContext.getContextPath());
+
+        // TO DO have to read the web.xml file and get the base url, class names.
+
+        baseUrl.append("/HAD_TO_GET_FROM_WEB.XML");
+
+        //scanning annotations
+        Reflections reflections = new Reflections("org.wso2.appserver.sample.service.CustomerService" ,
+                new MethodAnnotationsScanner(), new TypeAnnotationsScanner(), new SubTypesScanner());
+
+
+        Set<Class<?>> typesAnnotatedWith = reflections.getTypesAnnotatedWith(Path.class);
+
+        for (Class an :
+                typesAnnotatedWith) {
+            Path path = (Path) an.getAnnotation(Path.class);
+            baseUrl.append(path.value());
+        }
+
+        Set<Method> methods =
+                reflections.getMethodsAnnotatedWith(Path.class);
+        for (Method me :
+                methods) {
+            API api = new API(baseUrl.toString(), me);
+            log.info(api.toString());
+        }
     }
 
     @Override
