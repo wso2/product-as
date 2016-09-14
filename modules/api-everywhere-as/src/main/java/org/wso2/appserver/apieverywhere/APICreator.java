82ac3ae4d2dbeb00788e9af2525e7b871faada96
senthalan <senthalank@gmail.com>
2016-09-14 15:17:39 +0530

    adding keystore to SSL Certificate NOTE : have inter module dependency in appserver-utils

--- modules/api-everywhere-as/src/main/java/org/wso2/appserver/apieverywhere/APICreator.java
+++ modules/api-everywhere-as/src/main/java/org/wso2/appserver/apieverywhere/APICreator.java
@@ -6,15 +6,19 @@ import org.apache.juli.logging.LogFactory;
 import org.json.JSONObject;
 import org.wso2.appserver.apieverywhere.utils.APICreateRequest;
 import org.wso2.appserver.apieverywhere.utils.APIPath;
-import org.wso2.appserver.configuration.listeners.ServerConfigurationLoader;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -77,75 +81,43 @@ class APICreator extends Thread {
 
     private JSONObject httpCall(String encodedKey) {
         String requestAccessTokenUrl = "https://127.0.0.1:8243/token";
+        SSLSocketFactory sslSocketFactory = generateSSL();
+        if (sslSocketFactory == null) {
+            return null;
+        }
         try {
-            FileInputStream fis = null;
-            OutputStreamWriter os = null;
-            BufferedReader bufferedReader = null;
-            InputStreamReader inputStreamReader = null;
-            HttpsURLConnection connection = null;
-            try {
-                String keystoreLocation =  ServerConfigurationLoader.getServerConfiguration().
-                        getSecurityConfiguration().getKeystore().getLocation();
-        /* Load the keyStore that includes self-signed cert as a "trusted" entry. */
-                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                fis = new FileInputStream(keystoreLocation);
-                keyStore.load(fis, ServerConfigurationLoader.getServerConfiguration().
-                        getSecurityConfiguration().getKeystore().getPassword().toCharArray());
-                TrustManagerFactory tmf =
-                        TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-                tmf.init(keyStore);
-                SSLContext ctx = SSLContext.getInstance("TLS");
-                ctx.init(null, tmf.getTrustManagers(), null);
-                SSLSocketFactory sslFactory = ctx.getSocketFactory();
+            //Create connection
+            URL url = new URL(requestAccessTokenUrl);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Basic " + encodedKey);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setSSLSocketFactory(sslSocketFactory);
+            // for development purpose only
+            connection.setHostnameVerifier((hostname, sslSession) -> {
+                log.info("hostname: " + hostname);
+                return hostname.equals("127.0.0.1");
+            });
 
-
-                //Create connection
-                URL url = new URL(requestAccessTokenUrl);
-                connection = (HttpsURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Authorization", "Basic " + encodedKey);
-                connection.setDoOutput(true);
-                connection.setDoInput(true);
-                connection.setSSLSocketFactory(sslFactory);
-                connection.setHostnameVerifier((hostname, sslSession) -> {
-                    log.info("....................." + hostname);
-                    return hostname.equals("127.0.0.1");
-                });
-
-                os = new OutputStreamWriter(connection.getOutputStream(), "utf-8");
+            try (OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream(), "utf-8")) {
                 os.write("grant_type=password&username=admin&password=admin&scope=apim:api_create");
-                os.close();
+            }
 
-                int responseCode = connection.getResponseCode();
-                log.info("Status code " + responseCode);
-                inputStreamReader = new InputStreamReader(connection.getInputStream(), "utf-8");
-                bufferedReader = new BufferedReader(inputStreamReader);
-                StringBuilder stringBuilder = new StringBuilder();
+            log.info("Status code " + connection.getResponseCode());
+
+            StringBuilder stringBuilder = new StringBuilder();
+            try (InputStreamReader is = new InputStreamReader(connection.getInputStream(), "utf-8");
+                 BufferedReader bufferedReader = new BufferedReader(is)) {
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     stringBuilder.append(line);
                 }
-
-                return new JSONObject(stringBuilder.toString());
-            } finally {
-                if (fis != null) {
-                    fis.close();
-                }
-                if (os != null) {
-                    os.close();
-                }
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-                if (inputStreamReader != null) {
-                    inputStreamReader.close();
-                }
-                if (connection != null) {
-                    connection.disconnect();
-                }
             }
-        } catch (IOException | NoSuchAlgorithmException | KeyManagementException | KeyStoreException |
-                CertificateException e) {
+
+            connection.disconnect();
+            return new JSONObject(stringBuilder.toString());
+        } catch (IOException e) {
             log.error(e);
         }
         return null;
@@ -154,76 +126,74 @@ class APICreator extends Thread {
 
     private JSONObject createAPI(String accessToken, String apiJson) {
         String publishApiUrl = "https://127.0.0.1:9443/api/am/publisher/v0.10/apis";
+        SSLSocketFactory sslSocketFactory = generateSSL();
+        if (sslSocketFactory == null) {
+            return null;
+        }
+
         try {
-            FileInputStream fis = null;
-            OutputStreamWriter os = null;
-            BufferedReader bufferedReader = null;
-            InputStreamReader inputStreamReader = null;
-            HttpsURLConnection connection = null;
-            try {
-                String keystoreLocation =  ServerConfigurationLoader.getServerConfiguration().
-                        getSecurityConfiguration().getKeystore().getLocation();
-        /* Load the keyStore that includes self-signed cert as a "trusted" entry. */
-                KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-                fis = new FileInputStream(keystoreLocation);
-                keyStore.load(fis, "wso2carbon".toCharArray());
-                TrustManagerFactory tmf =
-                        TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-                tmf.init(keyStore);
-                SSLContext ctx = SSLContext.getInstance("TLS");
-                ctx.init(null, tmf.getTrustManagers(), null);
-                SSLSocketFactory sslFactory = ctx.getSocketFactory();
+            //Create connection
+            URL url = new URL(publishApiUrl);
+            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + accessToken);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setSSLSocketFactory(sslSocketFactory);
+            // for development purpose only
+            connection.setHostnameVerifier((hostname, sslSession) -> {
+                log.info("hostname: " + hostname);
+                return hostname.equals("127.0.0.1");
+            });
 
-
-                //Create connection
-                URL url = new URL(publishApiUrl);
-                connection = (HttpsURLConnection) url.openConnection();
-                connection.setRequestMethod("POST");
-                connection.setRequestProperty("Authorization", "Bearer " + accessToken);
-                connection.setRequestProperty("Content-Type", "application/json");
-                connection.setDoOutput(true);
-                connection.setDoInput(true);
-                connection.setSSLSocketFactory(sslFactory);
-                connection.setHostnameVerifier((hostname, sslSession) -> {
-                    log.info("....................." + hostname);
-                    return hostname.equals("127.0.0.1");
-                });
-
-                os = new OutputStreamWriter(connection.getOutputStream(), "utf-8");
+            try (OutputStreamWriter os = new OutputStreamWriter(connection.getOutputStream(), "utf-8")) {
                 os.write(apiJson);
-                os.close();
+            }
+            log.info("Status code " + connection.getResponseCode());
 
-                int responseCode = connection.getResponseCode();
-                log.info("Status code " + responseCode);
-                inputStreamReader = new InputStreamReader(connection.getInputStream(), "utf-8");
-                bufferedReader = new BufferedReader(inputStreamReader);
-                StringBuilder stringBuilder = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
+            try (InputStreamReader inputStreamReader = new InputStreamReader(connection.getInputStream(), "utf-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     stringBuilder.append(line);
                 }
-
-                return new JSONObject(stringBuilder.toString());
-            } finally {
-                if (fis != null) {
-                    fis.close();
-                }
-                if (os != null) {
-                    os.close();
-                }
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-                if (inputStreamReader != null) {
-                    inputStreamReader.close();
-                }
-                if (connection != null) {
-                    connection.disconnect();
-                }
             }
-        } catch (IOException | NoSuchAlgorithmException | KeyManagementException | KeyStoreException |
-                CertificateException e) {
+            connection.disconnect();
+            return new JSONObject(stringBuilder.toString());
+        } catch (IOException e) {
             log.error(e);
+        }
+        return null;
+    }
+
+    private SSLSocketFactory generateSSL() {
+        String keystorePathString = System.getProperty(org.wso2.appserver.Constants.JAVA_KEYSTORE_LOCATION);
+        String keystorePasswordString = System.getProperty(org.wso2.appserver.Constants.JAVA_KEYSTORE_PASSWORD);
+
+        Path keyStorePath = Paths.get(URI.create(keystorePathString).getPath());
+        if (Files.exists(keyStorePath)) {
+            try (InputStream keystoreInputStream = Files.newInputStream(keyStorePath)) {
+                KeyStore keyStore = KeyStore.getInstance(System.getProperty(org.wso2.appserver.Constants.
+                        JAVA_KEYSTORE_TYPE));
+                keyStore.load(keystoreInputStream, keystorePasswordString.toCharArray());
+
+                TrustManagerFactory tmf =
+                        TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+                tmf.init(keyStore);
+
+
+                SSLContext ctx = SSLContext.getInstance("TLS");
+                ctx.init(null, tmf.getTrustManagers(), null);
+
+                return ctx.getSocketFactory();
+            } catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException |
+                    KeyManagementException e) {
+                log.error(e);
+            }
+        } else {
+            log.error("File path specified for the keystore does not exist");
         }
         return null;
     }
