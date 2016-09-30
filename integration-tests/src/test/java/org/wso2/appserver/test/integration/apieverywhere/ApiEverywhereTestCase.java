3503125145218f8d4ec6631120c31a09afbe4008
senthalan <senthalank@gmail.com>
2016-09-30 15:32:06 +0530

    add simple test for deployment of jaxrs_basic web app

--- integration-tests/src/test/java/org/wso2/appserver/test/integration/apieverywhere/ApiEverywhereTestCase.java
+++ integration-tests/src/test/java/org/wso2/appserver/test/integration/apieverywhere/ApiEverywhereTestCase.java
@@ -1,18 +1,13 @@
 package org.wso2.appserver.test.integration.apieverywhere;
 
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.wso2.appserver.test.integration.TestBase;
 import org.wso2.appserver.test.integration.TestConstants;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.file.Paths;
 
 /**
  * This test case checks the creating api in API Publisher.
@@ -21,24 +16,16 @@ import java.nio.file.Paths;
  */
 public class ApiEverywhereTestCase extends TestBase {
 
-    private static final Logger log = LoggerFactory.getLogger(ApiEverywhereTestCase.class);
-
-    @Test(description = "test if the api creation working.......................")
+    @Test(description = "Test for jaxrs_basic-6.0.0-SNAPSHOT web app is running")
     public void testAPICreation() throws IOException {
-        File appserverHome = new File(System.getProperty(TestConstants.APPSERVER_HOME));
-        FileUtils.copyDirectory(
-                Paths.get(appserverHome.toString(), "samples", "jaxrs_basic", "target", "jaxrs_basic.war").
-                        toFile(), Paths.get(appserverHome.toString(), "webapps").toFile());
 
-        for (int i = 0; i < 100; i++) {
-            log.info("wating : " + i);
-        }
-        URL requestUrlGet = new URL(getBaseUrl() + "jaxrs_basic/services/customerservice/customers/123");
+        URL requestUrlGet = new URL(getBaseUrl() +
+                "/jaxrs_basic-6.0.0-SNAPSHOT/services/customerservice/customers/123");
         HttpURLConnection connectionGet = (HttpURLConnection) requestUrlGet.openConnection();
         connectionGet.setRequestMethod(TestConstants.HTTP_GET_METHOD);
 
         int responseCodeGet = connectionGet.getResponseCode();
-        Assert.assertEquals(responseCodeGet, 200, "Server Response Code");
+        Assert.assertEquals(responseCodeGet, 200, "Error in accessing the jaxrs_basic-6.0.0-SNAPSHOT web app");
     }
 
 }
