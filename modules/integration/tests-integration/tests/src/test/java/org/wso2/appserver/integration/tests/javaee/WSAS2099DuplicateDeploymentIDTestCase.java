46ef6183aa35ff38dde2b2dd620c7001b7ac07ad
Niranjan-K <niranjan.karu@gmail.com>
2017-12-06 17:23:49 +0530

    Fix code formating issue

--- modules/integration/tests-integration/tests/src/test/java/org/wso2/appserver/integration/tests/javaee/WSAS2099DuplicateDeploymentIDTestCase.java
+++ modules/integration/tests-integration/tests/src/test/java/org/wso2/appserver/integration/tests/javaee/WSAS2099DuplicateDeploymentIDTestCase.java
@@ -1,26 +1,25 @@
 /*
-* Copyright 2016 The Apache Software Foundation.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.appserver.integration.tests.javaee;
 
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Factory;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.carbon.automation.test.utils.common.TestConfigurationProvider;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
@@ -78,21 +77,21 @@ public class WSAS2099DuplicateDeploymentIDTestCase extends ASIntegrationTest {
 
     @Test(groups = "wso2.as", description = "Deploying duplicate web application")
     public void deployDuplicateWebappTest() throws Exception {
-
         if (userMode == TestUserMode.SUPER_TENANT_ADMIN) {
-            String[] carbonLogs = ASIntegrationLoggingUtil.getLogsFromLogfile(
-                    new File(System.getProperty("carbon.home") + File.separator + "repository" + File.separator + "logs" +
-                            File.separator + "wso2carbon.log"));
+            String[] carbonLogs = ASIntegrationLoggingUtil.getLogsFromLogfile(new File(System.getProperty(
+                    "carbon.home") + File.separator + "repository" + File.separator + "logs" + File.separator +
+                    "wso2carbon.log"));
             boolean isLogInLogsRecorded = false;
 
             for (String logEvent : carbonLogs) {
-                if (logEvent.contains("Application cannot be deployed as it contains deployment-ids which are in use")) {
+                if (logEvent.contains("Application cannot be deployed as it contains deployment-ids which are in " +
+                        "use")) {
                     isLogInLogsRecorded = true;
                     break;
                 }
             }
-            assertFalse(isLogInLogsRecorded,
-                    "Application cannot be deployed as it contains deployment-ids which are in use " + userMode);
+            assertFalse(isLogInLogsRecorded, "Application cannot be deployed as it contains deployment-ids which are " +
+                    "in use " + userMode);
         }
     }
 }
